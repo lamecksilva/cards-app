00f8dc6306efc358a1ae716ceef5e8920540e5ed
lamecksilva <lameck2@hotmail.com>
2019-04-29 00:18:31 -0300

    Comentarios no docker-compose.yml adicionados

--- index.js
+++ index.js
@@ -2,14 +2,25 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 
+// Instanciando o objeto express
 const app = express();
 
+// Middleware para 'parsing' do body
 app.use(bodyParser.json());
 
+// Endpoint "/" do servidor
 app.get("/", (req, res) => {
   res.json({ msg: "Success" });
 });
 
-const PORT = process.env.PORT || 9000
+// Conectando o mongodb a nossa aplicacao
+mongoose
+  .connect("mongo://db:27017/cards-app", { useNewUrlParser: true })
+  .then(response => console.log("MongoDB Connected"))
+  .catch(err => console.log(err));
 
-app.listen(PORT, ()=> console.log(`Server Running on port: ${PORT}`))
+// Declarando a porta
+const PORT = process.env.PORT || 9000;
+
+// "Listening" o servidor na porta
+app.listen(PORT, () => console.log(`Server Running on port: ${PORT}`));
