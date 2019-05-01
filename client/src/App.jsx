87e41596cd4f1bd8776a45917d7937169e716c60
lamecksilva <lameck2@hotmail.com>
2019-05-01 14:01:46 -0300

    Adicionar comentários e dockerfile do client

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 
+// Componente mais acima da aplicação, que será renderizado diretamente na div "root"
 const App = () => (
   <div>
     <h1 className="text-center mt-3">App.js</h1>
