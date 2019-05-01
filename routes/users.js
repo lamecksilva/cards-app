06b8136a2cf5ceff61d02b2fd9ba013ed01516a1
lamecksilva <lameck2@hotmail.com>
2019-05-01 09:04:11 -0300

    Adicionar validação de dados para cadastro de usuario

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,8 @@
 const express = require('express');
 // const mongoose = require("mongoose");
 
+const validateCadastroInput = require('../validation/cadastro');
+
 // Instanciando o objeto Router
 const router = express.Router();
 
@@ -9,6 +11,17 @@ const router = express.Router();
 // @access  Public
 router.get('/test', (req, res) => {
   res.send('Hello from /api/users/test');
+});
+
+// @Route   POST api/users/cadastro
+// @desc    Rota para cadastrar novos usuários no banco de dados
+// @access  Public
+router.post('/cadastro', (req, res) => {
+  const { errors, isValid } = validateCadastroInput(req.body);
+
+  if (!isValid) {
+    res.status(400).json(errors);
+  }
 });
 
 module.exports = router;
