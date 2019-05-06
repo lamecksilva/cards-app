7c51db569ac97dd7b7d450b868bfc63f318b17f8
Lameck <lameck2@hotmail.com>
2019-05-05 22:50:37 -0300

    Adicionar comentários sobre Provider, Actions

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,6 +6,8 @@ import Example from './components/Example';
 
 // Componente mais acima da aplicação, que será renderizado diretamente na div "root"
 const App = () => (
+  // O componente "Provider" faz com que a store do Redux seja acessivel em todos os
+  // componentes encadeados, apenas fazendo uso da função "connect()"
   <Provider store={store}>
     <Example />
   </Provider>
--- client/src/components/Example/Example.actions.js
+++ client/src/components/Example/Example.actions.js
@@ -1,9 +1,14 @@
-const actionExample = () => (dispatch) => {
+/*      ===========  Actions ===========
+  Redux "Actions" são apenas "objetos" onde possuem o "type" da action
+  e outro qualquer tipo de dado (ideal colocar em uma chave "payload"),
+  que vai para o reducer, onde "trata" essa action
+*/
+const actionExample = () => {
   console.log('Example Action');
 
-  dispatch({
+  return {
     type: 'TOGGLE',
-  });
+  };
 };
 
 export { actionExample };
