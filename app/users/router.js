dbb74e825951c80a65ef2c318a885b899b33c470
lamecksilva <lameck2@hotmail.com>
2019-05-23 22:33:31 -0300

    [feature] Adicionar rota para retornar todos usuários

--- app/users/router.js
+++ app/users/router.js
@@ -6,6 +6,36 @@ const controller = require('./controller');
 
 // Use o apidoc para gerar a documentação das rotas da API
 // gere documentação com: apidoc -i app/ -o apidoc/
+
+/**
+ *    @apiGroup User
+ *    @api {get} /users Retornando usuários do banco de dados
+ *    @apiExample {response} Exemplo de response:
+ *      {
+ *        "success": true,
+ *        "users": [
+ *          {
+ *            "cards": [],
+ *            "_id": "5cdb681d6863130046c90669",
+ *            "name": "Lameck Sandro",
+ *            "email": "lameck@lsdev.com",
+ *            "date": "2019-05-15T01:15:09.473Z",
+ *            "__v": 0
+ *          },
+ *          {
+ *            "cards": [],
+ *            "_id": "5cdb687707624a005318e921",
+ *            "name": "Lameck Sanders",
+ *            "email": "lamecksanders@lsdev.com",
+ *            "date": "2019-05-15T01:16:39.060Z",
+ *            "__v": 0
+ *          }
+ *        ]
+ *      }
+ *
+ */
+router.get('/', controller.getUsers);
+
 /**
  *    @apiGroup User
  *    @api {post} /register Adicionar usuário para o db
