7b7c87c00dbe3244dde0abd8463f40040c86128d
Lameck <lameck2@hotmail.com>
2019-06-01 19:06:51 -0300

    [feature] Adiciona documentação de rotas

--- app/cards/router.js
+++ app/cards/router.js
@@ -56,8 +56,57 @@ router.get('/', controller.getCards);
  */
 router.post('/register', upload.single('image'), controller.registerCard);
 
+/**
+ *  @apiGroup Card
+ *  @api {patch} api/update/:id Atualizar card
+ *  @apiParam {String} id ObjectId do card (Required)
+ *  @apiParam {String} title Título do Card (Required)
+ *  @apiParam {String} description Descrição do card (Optional)
+ *  @apiParam {String} user ObjectId do Usuário autor do card
+ *  @apiExample {request} Request Body (exemplo)
+ *    /api/update/5cf2f1689118660149af9002
+ *    {
+ *      "title": "Title Update",
+ *      "description": "Update description here"
+ *    }
+ *  @apiExample {response} Response (exemplo)
+ *    {
+ *      "success": true,
+ *      "card": {
+ *        "_id": "5cf2f1689118660149af9002",
+ *        "title": "Title Update",
+ *        "description": "Update description here",
+ *        "user": "5cd40e2389eef5018f5aa8fe",
+ *        "image": "images/1559425384954-5cd40e2389eef5018f5aa8fe.jpg",
+ *        "__v": 0
+ *      }
+ *    }
+ */
 router.patch('/update/:id', controller.editCard);
 
+/**
+ *  @apiGroup Card
+ *  @api {put} api/update-image/:id Atualizar imagem de card
+ *  @apiParam {String} id ObjectId do card (Required)
+ *  @apiParam {File} image  Imagem do card (Required)
+ *  @apiExample {request} Request Multipart Form Data (exemplo)
+ *    /api/update-image/5cf2f1689118660149af9002
+ *    {
+ *      "image": "file here",
+ *    }
+ *  @apiExample {response} Response (exemplo)
+ *    {
+ *      "success": true,
+ *      "card": {
+ *        "_id": "5cf2f1689118660149af9002",
+ *        "title": "Title Update",
+ *        "description": "Update description here",
+ *        "user": "5cd40e2389eef5018f5aa8fe",
+ *        "image": "images/1559425384954-5cd40e2389eef5018f5aa8fe.jpg",
+ *        "__v": 0
+ *      }
+ *    }
+ */
 router.put('/update-image/:id', upload.single('image'), controller.updateImage);
 
 module.exports = router;
