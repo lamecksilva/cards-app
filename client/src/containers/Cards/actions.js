8a82507fbcc2ee901bcddfa0309d1841f2af291c
Lameck <lameck2@hotmail.com>
2019-06-04 08:58:50 -0300

    [feature] Adiciona dispatch para erro e loading

--- client/src/containers/Cards/actions.js
+++ client/src/containers/Cards/actions.js
@@ -1,8 +1,12 @@
 import axios from 'axios';
 
-import { GET_CARDS_SUCCESS } from './actionTypes';
+import { GET_CARDS_SUCCESS, GET_CARDS_ERROR, GET_CARDS_LOADING } from './actionTypes';
 
 export const getCards = () => (dispatch) => {
+  dispatch({
+    type: GET_CARDS_LOADING
+  })
+  
   axios
     .get('/api/cards')
     .then((response) => {
@@ -12,5 +16,11 @@ export const getCards = () => (dispatch) => {
         payload: response.data,
       });
     })
-    .catch(error => console.log(error));
+    .catch((error) => {
+      console.log(error.response);
+
+      dispatch({
+        type: GET_CARDS_ERROR,
+      });
+    });
 };
--- client/src/containers/Cards/reducer.js
+++ client/src/containers/Cards/reducer.js
@@ -1,15 +1,32 @@
-import { GET_CARDS_SUCCESS } from './actionTypes';
+import { GET_CARDS_SUCCESS, GET_CARDS_LOADING, GET_CARDS_ERROR } from './actionTypes';
 
 const initialState = {
   cards: [],
+  loading: false,
+  error: false,
 };
 
 export default (state = initialState, action) => {
   switch (action.type) {
+    case GET_CARDS_LOADING:
+      return {
+        ...state,
+        loading: true,
+      };
+
     case GET_CARDS_SUCCESS:
       return {
         ...state,
         cards: action.payload.cards,
+        loading: false,
+        error: false,
+      };
+
+    case GET_CARDS_ERROR:
+      return {
+        ...state,
+        loading: false,
+        error: true,
       };
 
     default:
